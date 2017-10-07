3e02c1a4274bfb9a0bc3e8d5f48389581e998d2e
mathix <mathixx@gmail.com>
2017-10-08 00:15:25 +0200

    Calculating center of mass

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -5,6 +5,25 @@ import Planet from "./planet";
 
 let planets : Planet[];
 
+let centerOfMassSprite : PIXI.Graphics;
+
+function createCrosshairSprite() {
+    let size = 50;
+
+    let centerOfMassSprite = new Graphics();
+    // centerOfMassSprite.beginFill(0xFFFFFF);
+    // centerOfMassSprite.drawRect(0, 0, 5, 5);
+    // centerOfMassSprite.endFill();
+    centerOfMassSprite.lineStyle(1,0xFFFFFF,0.5);
+    centerOfMassSprite.moveTo(-size/2, 0);
+    centerOfMassSprite.lineTo(size/2, 0);
+    centerOfMassSprite.moveTo(0, -size/2);
+    centerOfMassSprite.lineTo(0, size/2);
+    centerOfMassSprite.drawCircle(0,0,size/4);
+
+    return centerOfMassSprite;
+}
+
 function setup(stage: PIXI.Container){
     let planet1 = new Planet(new Vector(250, 250),new Vector(0,0), 100, 0xFF0000);
     let planet2 = new Planet(new Vector(300, 200),new Vector(0.001,0.01), 1, 0x00FF00);
@@ -13,6 +32,10 @@ function setup(stage: PIXI.Container){
     stage.addChild(planet2.sprite);
 
     planets = [planet1, planet2];
+
+    centerOfMassSprite = createCrosshairSprite();
+
+    stage.addChild(centerOfMassSprite);
 }
 
 function startGameLoop(stage: PIXI.Container, renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer){
@@ -22,9 +45,21 @@ function startGameLoop(stage: PIXI.Container, renderer: PIXI.WebGLRenderer | PIX
 
     function gameLoop(timestamp : number){
         let timestampDelta = lastTimestamp == undefined ? 0 : (timestamp - lastTimestamp);
-        let scaledTimestampDelta = timestampDelta * 10;
+        let scaledTimestampDelta = timestampDelta * 50;
 
         planets.forEach(p => p.update(scaledTimestampDelta, planets.filter(fp => fp != p)));
+
+        let sumOfMassPositions = planets.reduce((acc, current) => {
+            let positionScaledByMass = current.position.multiply(current.mass);
+            return positionScaledByMass.add(acc);
+        }, new Vector(0,0));
+
+        let sumOfMasses = planets.reduce((acc, current) => {
+            return acc + current.mass;
+        },0);
+
+        let centerOfMass = sumOfMassPositions.multiply(1/sumOfMasses);
+        centerOfMassSprite.position = centerOfMass.toPoint();
 
         renderer.render(stage);
 
--- src/scripts/planet.ts
+++ src/scripts/planet.ts
@@ -3,40 +3,51 @@ import Graphics = PIXI.Graphics;
 import Vector from "./vector";
 
 const G = 6.67 * Math.pow(10, -5);
+const maxTimestampDelta = 5;
 
 // dv = G * (m2 / (r * r)) * e_versor * dt
 
 class Planet{
-    private readonly mass: number;
+    public readonly mass: number;
     readonly sprite : Graphics;
-    protected position : Vector;
-    protected velocity : Vector;
+    public position : Vector;
+    public velocity : Vector;
 
     constructor(position: Vector, velocity: Vector, mass: number, color: number){
         this.position = position;
         this.velocity = velocity;
         this.mass = mass;
 
+        let radius = Math.pow(mass * 10, 1/3);
+
         var sprite = new Graphics();
         sprite.beginFill(color);
-        sprite.drawCircle(0, 0, 12);
+        sprite.drawCircle(0, 0, radius);
         sprite.endFill();
         sprite.position = position.toPoint();
         this.sprite = sprite;
     }
 
     update(timestampDelta: number, otherPlanets: Planet[]){
-        let dvSum = otherPlanets.reduce((acc, p) => {
-            let thisToOther = p.position.subtract(this.position);
-            let r = thisToOther.length();
-            let dv = thisToOther.toVersor().multiply(G * (p.mass / (r * r)) * timestampDelta);
-            return dv.add(acc);
-        }, new Vector(0,0));
+
+        let timestampLeft = timestampDelta;
+
+        while(timestampLeft > 0){
+            let timestampDeltaForStep = Math.min(timestampLeft, maxTimestampDelta);
+            timestampLeft -= timestampDeltaForStep;
+
+            let dvSum = otherPlanets.reduce((acc, p) => {
+                let thisToOther = p.position.subtract(this.position);
+                let r = thisToOther.length();
+                let dv = thisToOther.toVersor().multiply(G * (p.mass / (r * r)) * timestampDeltaForStep);
+                return dv.add(acc);
+            }, new Vector(0,0));
+
+            this.velocity = this.velocity.add(dvSum);
+            this.position = this.position.add(this.velocity.multiply(timestampDeltaForStep));
+        }
 
 
-        this.velocity = this.velocity.add(dvSum);
-
-        this.position = this.position.add(this.velocity.multiply(timestampDelta));
         this.sprite.position = this.position.toPoint();
     }
 }
