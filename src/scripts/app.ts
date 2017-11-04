d6fa025da6464af1fddffba8b23a5a0e92e767a9
mathix <mathixx@gmail.com>
2017-11-04 20:57:33 +0100

    Refactoring code

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,33 +1,33 @@
 import '../styles/base.scss';
 import * as PIXI from 'pixi.js';
-import startGameLoop from './game';
+import Game from './game';
 import '../styles/base.scss';
 import {initializeControls} from './controls/controls';
 
-initializeControls();
-
-let renderer = PIXI.autoDetectRenderer(512, 512, {transparent: false});
-
-let rendererHolder = document.getElementById('renderer');
-rendererHolder.appendChild(renderer.view);
-
-(renderer as any).backgroundColor = 0x000000;
-let stage = new PIXI.Container();
-
-renderer.render(stage);
-
 window.addEventListener('resize', (e)=> {
 
-})
+});
 
 function setup(){
-    startGameLoop(stage, renderer);
+    // startGameLoop(stage, renderer);
     // function gameLoop(timestamp : number){
     //   renderer.render(stage);
     //   requestAnimationFrame(gameLoop);
     // }
     //
     // requestAnimationFrame(gameLoop);
+    initializeControls();
+
+    let renderer = PIXI.autoDetectRenderer(512, 512, {transparent: false});
+
+    let rendererHolder = document.getElementById('renderer');
+    rendererHolder.appendChild(renderer.view);
+
+    (renderer as any).backgroundColor = 0x000000;
+    let stage = new PIXI.Container();
+
+    renderer.render(stage);
+
 }
 
 // PIXI.loader.load(setup);
--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -3,75 +3,77 @@ import Graphics = PIXI.Graphics;
 import {Vector} from "./vector";
 import Planet from "./planet";
 
-let planets : Planet[];
+class Game {
+    private planets : Planet[];
+    private centerOfMassSprite : PIXI.Graphics;
 
-let centerOfMassSprite : PIXI.Graphics;
+    constructor(stage: PIXI.Container){
 
-function createCrosshairSprite() {
-    let size = 50;
+    }
 
-    let centerOfMassSprite = new Graphics();
-    // centerOfMassSprite.beginFill(0xFFFFFF);
-    // centerOfMassSprite.drawRect(0, 0, 5, 5);
-    // centerOfMassSprite.endFill();
-    centerOfMassSprite.lineStyle(1,0xFFFFFF,0.5);
-    centerOfMassSprite.moveTo(-size/2, 0);
-    centerOfMassSprite.lineTo(size/2, 0);
-    centerOfMassSprite.moveTo(0, -size/2);
-    centerOfMassSprite.lineTo(0, size/2);
-    centerOfMassSprite.drawCircle(0,0,size/4);
+    createCrosshairSprite() {
+        let size = 50;
 
-    return centerOfMassSprite;
-}
+        let centerOfMassSprite = new Graphics();
+        centerOfMassSprite.lineStyle(1,0xFFFFFF,0.5);
+        centerOfMassSprite.moveTo(-size/2, 0);
+        centerOfMassSprite.lineTo(size/2, 0);
+        centerOfMassSprite.moveTo(0, -size/2);
+        centerOfMassSprite.lineTo(0, size/2);
+        centerOfMassSprite.drawCircle(0,0,size/4);
 
-function setup(stage: PIXI.Container){
-    let planet1 = new Planet(new Vector(250, 250),new Vector(0,0), 100, 0xFF0000);
-    let planet2 = new Planet(new Vector(300, 200),new Vector(0.001,0.01), 1, 0x00FF00);
+        return centerOfMassSprite;
+    }
 
-    stage.addChild(planet1.sprite);
-    stage.addChild(planet2.sprite);
+    setup(stage: PIXI.Container){
+        let planet1 = new Planet(new Vector(250, 250),new Vector(0,0), 100, 0xFF0000);
+        let planet2 = new Planet(new Vector(300, 200),new Vector(0.001,0.01), 1, 0x00FF00);
 
-    planets = [planet1, planet2];
+        stage.addChild(planet1.sprite);
+        stage.addChild(planet2.sprite);
 
-    centerOfMassSprite = createCrosshairSprite();
+        this.planets = [planet1, planet2];
 
-    stage.addChild(centerOfMassSprite);
-}
+        this.centerOfMassSprite = this.createCrosshairSprite();
 
-function startGameLoop(stage: PIXI.Container, renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer){
-    setup(stage);
+        stage.addChild(this.centerOfMassSprite);
+    }
 
-    let lastTimestamp : number = undefined;
+     startGameLoop(stage: PIXI.Container, renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer){
+        this.setup(stage);
 
-    function gameLoop(timestamp : number){
-        let timestampDelta = lastTimestamp == undefined ? 0 : (timestamp - lastTimestamp);
-        let scaledTimestampDelta = timestampDelta * 50;
+        let lastTimestamp : number = undefined;
 
-        planets.forEach(p => p.update(scaledTimestampDelta, planets.filter(fp => fp != p)));
+        let gameLoop = (timestamp : number) =>{
+            let timestampDelta = lastTimestamp == undefined ? 0 : (timestamp - lastTimestamp);
+            let scaledTimestampDelta = timestampDelta * 50;
 
-        let sumOfMassPositions = planets.reduce((acc, current) => {
-            let positionScaledByMass = current.position.multiply(current.mass);
-            return positionScaledByMass.add(acc);
-        }, new Vector(0,0));
+            this.planets.forEach(p => p.update(scaledTimestampDelta, this.planets.filter(fp => fp != p)));
 
-        let sumOfMasses = planets.reduce((acc, current) => {
-            return acc + current.mass;
-        },0);
+            let sumOfMassPositions = this.planets.reduce((acc, current) => {
+                let positionScaledByMass = current.position.multiply(current.mass);
+                return positionScaledByMass.add(acc);
+            }, new Vector(0,0));
 
-        let centerOfMass = sumOfMassPositions.multiply(1/sumOfMasses);
+            let sumOfMasses = this.planets.reduce((acc, current) => {
+                return acc + current.mass;
+            },0);
 
-        centerOfMassSprite.position = centerOfMass.toPoint();
+            let centerOfMass = sumOfMassPositions.multiply(1/sumOfMasses);
 
-        stage.pivot = centerOfMass.subtract(new Vector(renderer.width/2,renderer.height/2)).toPoint(); // subtract(new Vector(stage.width / 2, stage.height / 2)).toPoint();
+            this.centerOfMassSprite.position = centerOfMass.toPoint();
 
-        renderer.render(stage);
+            stage.pivot = centerOfMass.subtract(new Vector(renderer.width/2,renderer.height/2)).toPoint(); // subtract(new Vector(stage.width / 2, stage.height / 2)).toPoint();
 
-        lastTimestamp = timestamp;
+            renderer.render(stage);
+
+            lastTimestamp = timestamp;
+
+            requestAnimationFrame(gameLoop);
+        }
 
         requestAnimationFrame(gameLoop);
     }
-
-    requestAnimationFrame(gameLoop);
 }
 
-export default startGameLoop;
+export default Game;
